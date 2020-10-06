492894814ab7d11bc6a297f3a14a2c899568d462
Ben Morgan <ben.morgan@warwick.ac.uk>
2020-10-06 10:08:50 +0100

    Create CopCore subproject
    Keep coprocessor tools/types in-house whilst allowing for future
    export to VecCore-like dedicated repository. Copy major infrastructure
    such as license etc from core AdePT. Implement CMake scripting such that
    it works both as a subproject or a separate one in the future.

---
+++ base/inc/CopCore/include/CopCore/CopCore.h
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2020 CERN
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * @file CopCore/CopCore.h
+ * @brief Main front end to CopCore
+ */
+
+#ifndef COPCORE_COPCORE_H_
+#define COPCORE_COPCORE_H_
+
+#endif // COPCORE_COPCORE_H_
