0ab0ab5f9c4d2e8bd11831aa3b0f460b8432e3bc
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2020-10-09 13:40:20 +0000

    Add skeleton of unit tests executable. Update CMakeLists.txt to build the above.

---
+++ test/FisherPrice_Alpaka/unitTests_alpaka_fisher_price.cu
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2020 CERN
+// SPDX-License-Identifier: Apache-2.0
+
+// This is the main include file to give us all of the alpaka classes, functions etc.
+#include <alpaka/alpaka.hpp>
+
+int main()
+{
+  return 0;
+}
