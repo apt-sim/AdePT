95643d31eaef8424d54ef4e759ecf8ffc1e623eb
Mark Hodgkinson <m.hodgkinson@sheffield.ac.uk>
2020-10-13 10:18:16 +0000

    Add some more comments explaining what the tests do. Migrate the testSplitParticle to use the initialMomentum variable, such that this momentum is now defined in one place only.

--- test/FisherPrice_Alpaka/unitTests_alpaka_fisher_price.cu
+++ test/FisherPrice_Alpaka/unitTests_alpaka_fisher_price.cu
@@ -91,14 +91,15 @@ int main()
   auto d_ELoss = mem::buf::alloc<float, Idx>(device, bufferExtent);
   auto h_ELoss = mem::buf::alloc<float, Idx>(devHost, bufferExtent);
 
-  // Create a task for processEvent, that we can run and then run it via a queue
+  // Create a task for testEnergyLoss, that we can run and then run it via a queue
   testEnergyLoss testEnergyLoss;
-  auto taskRunTestEnergyLoss = kernel::createTaskKernel<Acc>(workDiv, testEnergyLoss, mem::view::getPtrNative(d_event), mem::view::getPtrNative(d_ELoss));
-                                                           
+  auto taskRunTestEnergyLoss = kernel::createTaskKernel<Acc>(workDiv, testEnergyLoss, mem::view::getPtrNative(d_event), mem::view::getPtrNative(d_ELoss));                                                           
   queue::enqueue(queue, taskRunTestEnergyLoss);
-
+  //copy the calculated energy losses back to the host.
   mem::view::copy(queue, h_ELoss, d_ELoss, bufferExtent);
 
+  //Then we test the output of energyLoss for each thread.
+  //By construction the energy loss should not be more than 0.2 MeV.
   bool testOK = true;
   for (unsigned int counter = 0; counter < NPART; counter++){
 
@@ -107,23 +108,26 @@ int main()
 
   }
 
+  //Print the test result.
   std::cout << "Status of testEnergyLoss is " << testOK << std::endl;
 
   //Create data for testing particleProcessor.splitParticle
   auto d_newPart = mem::buf::alloc<part, Idx>(device, bufferExtent);
   auto h_newPart = mem::buf::alloc<part, Idx>(devHost, bufferExtent);
 
+  //Create a task for testSplitParticle, that we can run and then run it via a queue
   testSplitParticle testSplitParticle;
   auto taskRunTestSplitParticle = kernel::createTaskKernel<Acc>(workDiv, testSplitParticle, mem::view::getPtrNative(d_event), mem::view::getPtrNative(d_newPart));
-
   queue::enqueue(queue, taskRunTestSplitParticle);
-
+  //copy the part objects back to the host
   mem::view::copy(queue, h_newPart, d_newPart, bufferExtent);
 
+  //Then we test the output part for each thread.
+  //By construction the particle momentum should not be more than the initial momentum.
   testOK = true;
   for (unsigned int counter = 0; counter < NPART; counter++){
     part newPart = mem::view::getPtrNative(h_newPart)[counter];
-    if ( (newPart.getMom().length()) > 20000.) testOK = false;
+    if ( (newPart.getMom().length()) > initialMomentum) testOK = false;
   }
 
   std::cout << "Status of testSplitParticle is " << testOK << std::endl; 
