4637c5efdf1111bd424a4256a270c76fc446d35a
Witold Pokorski <witoldp@omenrtx.dyndns.cern.ch>
2020-11-30 11:03:20 +0100

    applying clang-format

--- physics/processes/inc/process_list.h
+++ physics/processes/inc/process_list.h
@@ -7,29 +7,36 @@
 #include "process.h"
 
 class process_list {
-    public:
-        __device__ process_list() {}
-        __device__ process_list(process **l, int n) {list = l; list_size = n; }
-        __device__ virtual bool GetPhysicsInteractionLength(int particle_index, adept::BlockData<track> *block, curandState_t *states) const;
-        process **list;
-        int list_size;
+public:
+  __device__ process_list() {}
+  __device__ process_list(process **l, int n)
+  {
+    list      = l;
+    list_size = n;
+  }
+  __device__ virtual bool GetPhysicsInteractionLength(int particle_index, adept::BlockData<track> *block,
+                                                      curandState_t *states) const;
+  process **list;
+  int list_size;
 };
 
-__device__ bool process_list::GetPhysicsInteractionLength(int particle_index, adept::BlockData<track> *block, curandState_t *states) const {
-        
-    bool physics_wins = false;
-    float current_length = FLT_MAX;
+__device__ bool process_list::GetPhysicsInteractionLength(int particle_index, adept::BlockData<track> *block,
+                                                          curandState_t *states) const
+{
 
-    for (int i = 0; i < list_size; i++) {
-        float temp = list[i]->GetPhysicsInteractionLength(particle_index, block, states);
-        if (temp < current_length) {
-                (*block)[particle_index].interaction_length = temp;
-                (*block)[particle_index].current_process = i;
-                physics_wins = true;
-                current_length = temp;
-            }
+  bool physics_wins    = false;
+  float current_length = FLT_MAX;
+
+  for (int i = 0; i < list_size; i++) {
+    float temp = list[i]->GetPhysicsInteractionLength(particle_index, block, states);
+    if (temp < current_length) {
+      (*block)[particle_index].interaction_length = temp;
+      (*block)[particle_index].current_process    = i;
+      physics_wins                                = true;
+      current_length                              = temp;
     }
-  return physics_wins; 
+  }
+  return physics_wins;
 }
 
 #endif
