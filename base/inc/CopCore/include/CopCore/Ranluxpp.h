618c3d1436ef0536ec606e730b5dcf114d256777
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2020-11-30 12:50:04 +0100

    RANLUX++ for host and device
    Copied from ROOT, with support for only one luxury level (p=2048).
    The main interface for now is RanluxppDouble, but we can implement
    more classes that use less random bits to produce a float.
    The added test shows how the state can be transferred between host
    and device via a cudaMemcpy and checks that the generated numbers
    on host and device are identical.

---
+++ base/inc/CopCore/include/CopCore/Ranluxpp.h
@@ -0,0 +1,149 @@
+// SPDX-FileCopyrightText: 2020 CERN
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifndef COPCORE_RANLUXPP_H_
+#define COPCORE_RANLUXPP_H_
+
+#include <VecCore/VecCore>
+
+#include "mulmod.h"
+
+#include <cassert>
+#include <cstdint>
+
+namespace {
+
+VECCORE_ATT_DEVICE
+const uint64_t kA_2048[] = {
+    0xed7faa90747aaad9, 0x4cec2c78af55c101, 0xe64dcb31c48228ec, 0x6d8a15a13bee7cb0, 0x20b2ca60cb78c509,
+    0x256c3d3c662ea36c, 0xff74e54107684ed2, 0x492edfcc0cc8e753, 0xb48c187cf5b22097,
+};
+
+} // end anonymous namespace
+
+template <int w>
+class RanluxppEngineImpl {
+
+private:
+  uint64_t fState[9]; ///< State of the generator
+  int fPosition = 0;  ///< Current position in bits
+
+  static constexpr const uint64_t *kA = kA_2048;
+  static constexpr int kMaxPos        = 9 * 64;
+
+  /// Produce next block of random bits
+  VECCORE_ATT_HOST_DEVICE
+  void Advance()
+  {
+    mulmod(kA, fState);
+    fPosition = 0;
+  }
+
+public:
+  RanluxppEngineImpl() = default;
+
+  /// Return the next random bits, generate a new block if necessary
+  VECCORE_ATT_HOST_DEVICE
+  uint64_t NextRandomBits()
+  {
+    if (fPosition + w > kMaxPos) {
+      Advance();
+    }
+
+    int idx     = fPosition / 64;
+    int offset  = fPosition % 64;
+    int numBits = 64 - offset;
+
+    uint64_t bits = fState[idx] >> offset;
+    if (numBits < w) {
+      bits |= fState[idx + 1] << numBits;
+    }
+    bits &= ((uint64_t(1) << w) - 1);
+
+    fPosition += w;
+    assert(fPosition <= kMaxPos && "position out of range!");
+
+    return bits;
+  }
+
+  /// Initialize and seed the state of the generator
+  VECCORE_ATT_HOST_DEVICE
+  void SetSeed(uint64_t s)
+  {
+    fState[0] = 1;
+    for (int i = 1; i < 9; i++) {
+      fState[i] = 0;
+    }
+
+    uint64_t a_seed[9];
+    // Skip 2 ** 96 states.
+    powermod(kA, a_seed, uint64_t(1) << 48);
+    powermod(a_seed, a_seed, uint64_t(1) << 48);
+    // Skip another s states.
+    powermod(a_seed, a_seed, s);
+    mulmod(a_seed, fState);
+
+    fPosition = 0;
+  }
+
+  /// Skip `n` random numbers without generating them
+  VECCORE_ATT_HOST_DEVICE
+  void Skip(uint64_t n)
+  {
+    int left = (kMaxPos - fPosition) / w;
+    assert(left >= 0 && "position was out of range!");
+    if (n < (uint64_t)left) {
+      // Just skip the next few entries in the currently available bits.
+      fPosition += n * w;
+      assert(fPosition <= kMaxPos && "position out of range!");
+      return;
+    }
+
+    n -= left;
+    // Need to advance and possibly skip over blocks.
+    int nPerState = kMaxPos / w;
+    int skip      = (n / nPerState);
+
+    uint64_t a_skip[9];
+    powermod(kA, a_skip, skip + 1);
+    mulmod(a_skip, fState);
+
+    // Potentially skip numbers in the freshly generated block.
+    int remaining = n - skip * nPerState;
+    assert(remaining >= 0 && "should not end up at a negative position!");
+    fPosition = remaining * w;
+    assert(fPosition <= kMaxPos && "position out of range!");
+  }
+};
+
+class RanluxppDouble : public RanluxppEngineImpl<52> {
+public:
+  VECCORE_ATT_HOST_DEVICE
+  RanluxppDouble(uint64_t seed = 314159265) { this->SetSeed(seed); }
+
+  VECCORE_ATT_HOST_DEVICE
+  double Rndm() { return (*this)(); }
+
+  VECCORE_ATT_HOST_DEVICE
+  double operator()()
+  {
+    // Get 52 bits of randomness.
+    uint64_t bits = this->NextRandomBits();
+
+    // Construct the double in [1, 2), using the random bits as mantissa.
+    static constexpr uint64_t exp = 0x3ff0000000000000;
+    union {
+      double dRandom;
+      uint64_t iRandom;
+    };
+    iRandom = exp | bits;
+
+    // Shift to the right interval of [0, 1).
+    return dRandom - 1;
+  }
+
+  VECCORE_ATT_HOST_DEVICE
+  uint64_t IntRndm() { return this->NextRandomBits(); }
+};
+
+#endif // COPCORE_RANLUXPP_H_
---
+++ test/test_ranluxpp.cu
@@ -0,0 +1,77 @@
+// SPDX-FileCopyrightText: 2020 CERN
+// SPDX-License-Identifier: Apache-2.0
+
+#include <CopCore/Ranluxpp.h>
+
+#include <iostream>
+
+__global__ void kernel(RanluxppDouble *r, double *d, uint64_t *i, double *d2)
+{
+  *d = r->Rndm();
+  *i = r->IntRndm();
+  r->Skip(42);
+  *d2 = r->Rndm();
+}
+
+int main(void)
+{
+  RanluxppDouble r;
+  std::cout << "double: " << r.Rndm() << std::endl;
+  std::cout << "int: " << r.IntRndm() << std::endl;
+
+  RanluxppDouble *r_dev;
+  cudaMalloc(&r_dev, sizeof(RanluxppDouble));
+  double *d_dev_ptr;
+  uint64_t *i_dev_ptr;
+  double *d2_dev_ptr;
+  cudaMalloc(&d_dev_ptr, sizeof(double));
+  cudaMalloc(&i_dev_ptr, sizeof(uint64_t));
+  cudaMalloc(&d2_dev_ptr, sizeof(double));
+
+  // Transfer the state of the generator to the device.
+  cudaMemcpy(r_dev, &r, sizeof(RanluxppDouble), cudaMemcpyHostToDevice);
+  cudaDeviceSynchronize();
+
+  kernel<<<1, 1>>>(r_dev, d_dev_ptr, i_dev_ptr, d2_dev_ptr);
+  cudaDeviceSynchronize();
+
+  // Generate from the same state on the host.
+  double d   = r.Rndm();
+  uint64_t i = r.IntRndm();
+  r.Skip(42);
+  double d2 = r.Rndm();
+
+  // Fetch the numbers from the device for comparison.
+  double d_dev;
+  uint64_t i_dev;
+  double d2_dev;
+  cudaMemcpy(&d_dev, d_dev_ptr, sizeof(double), cudaMemcpyDeviceToHost);
+  cudaMemcpy(&i_dev, i_dev_ptr, sizeof(uint64_t), cudaMemcpyDeviceToHost);
+  cudaMemcpy(&d2_dev, d2_dev_ptr, sizeof(double), cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
+
+  int ret = 0;
+
+  std::cout << std::endl;
+  std::cout << "double:" << std::endl;
+  std::cout << "   host:   " << d << std::endl;
+  std::cout << "   device: " << d_dev << std::endl;
+  ret += (d != d_dev);
+
+  std::cout << "int:" << std::endl;
+  std::cout << "   host:   " << i << std::endl;
+  std::cout << "   device: " << i_dev << std::endl;
+  ret += (i != i_dev);
+
+  std::cout << "double (after calling Skip(42)):" << std::endl;
+  std::cout << "   host:   " << d2 << std::endl;
+  std::cout << "   device: " << d2_dev << std::endl;
+  ret += (d2 != d2_dev);
+
+  cudaFree(r_dev);
+  cudaFree(d_dev_ptr);
+  cudaFree(i_dev_ptr);
+  cudaFree(d2_dev_ptr);
+
+  return ret;
+}
