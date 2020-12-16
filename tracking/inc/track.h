e4a3b1efef756fb1c0837247b0353226dfae15d1
John Apostolakis <john.apostolakis@cern.ch>
2020-12-16 13:17:18 +0100

    track: Added method for mass;  made pdg & process id 'short'
    Including PhysicalConstants for value of electron mass.
    
    New methods are host & device.

--- tracking/inc/track.h
+++ tracking/inc/track.h
@@ -5,6 +5,7 @@
 #define TRACKH
 
 #include <CopCore/Ranluxpp.h>
+#include <CopCore/PhysicalConstants.h>
 
 #include <VecGeom/base/Vector3D.h>
 #include <VecGeom/navigation/NavStateIndex.h>
@@ -16,7 +17,6 @@ enum TrackStatus { alive, dead };
 struct track {
   RanluxppDouble rng_state;
   int index{0};
-  int pdg{0};
   double energy{10};
   vecgeom::Vector3D<double> pos;
   vecgeom::Vector3D<double> dir;
@@ -24,15 +24,21 @@ struct track {
   vecgeom::NavStateIndex next_state;
   int mother_index{0};
   TrackStatus status{alive};
-  int current_process{0};
+
   float interaction_length{FLT_MAX};
   float energy_loss{0};         // primitive version of scoring
   int number_of_secondaries{0}; // primitive version of scoring
 
+  short pdg{0};               // Enough for e/g, even p/n/pi/K
+  short current_process{0};   // 
+   
   __device__ double uniform() { return rng_state.Rndm(); }
 
-  __device__ __host__ int charge()
+  __device__ __host__ int    charge()
   { int chrg= (pdg== -11)-(pdg==11); return chrg; }
+
+  __device__ __host__ double mass()  // Rest mass
+  { return  (pdg== 22) ? 0 : copcore::units::kElectronMassC2; } 
    
   __device__ void SwapStates()
   {
