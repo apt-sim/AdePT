8e3cdcb90243c5c830cd6b70abf76a7d638ca304
John Apostolakis <john.apostolakis@cern.ch>
2020-12-17 11:36:23 +0100

    ConstBzFieldStepper: cleanup and minor changes.

--- tracking/inc/ConstBzFieldStepper.h
+++ tracking/inc/ConstBzFieldStepper.h
@@ -10,17 +10,10 @@
 #ifndef CONSTBzFIELDSTEPPER_H_
 #define CONSTBzFIELDSTEPPER_H_
 
-// #include <Geant/Config.h>
 #include "VecGeom/base/Global.h"
-// #include <Geant/VectorTypes.h>
 
-// #include "VecGeom/base/Vector3D.h"
-//  May not be needed ... ?   JA 12.11.2020
-//
-//  Later change to <>
-
-//namespace geant {
-// inline namespace GEANT_IMPL_NAMESPACE {
+//namespace adept {
+// inline namespace ADEPT_IMPL_NAMESPACE {
 
 /**
  * A very simple stepper treating the propagation of particles in a constant Bz magnetic field
@@ -38,6 +31,10 @@ public:
   void SetBz(float Bz) { fBz = Bz; }
   double GetBz() const { return fBz; }
 
+  static constexpr float kB2C =
+     -0.299792458 * (copcore::units::GeV /
+                     (copcore::units::tesla * copcore::units::meter) ) ;
+   
   /*
   template<typename RT, typename Vector3D>
   RT GetCurvature( Vector3D const & dir,
@@ -62,23 +59,6 @@ public:
       ) const;
 
   /**
-   * basket version of dostep
-   * version that takes plain arrays as input; suited for current Geant-V
-   *
-   * SW: for the moment (12.5.2015) commenting this out as not used
-   *
-   */
-  // void DoStep_v( double const * /*posx*/, double const * /*posy*/, double const * /*posz*/,
-  //               double const * /*dirx*/, double const * /*diry*/, double const * /*dirz*/,
-  //              int const * /*charge*/, double const * /*momentum*/, double const * /*step*/,
-  //              double * /*newsposx*/, double * /*newposy*/, double * /*newposz*/,
-  //              double * /*newdirx*/, double * /*newdiry*/, double * /*newdirz*/,
-  //              int np
-  //           ) const ;
-
-  // in future will offer versions that take containers as input
-
-  /**
    * this function propagates the track along the helix solution by a step
    * input: current position, current direction, some particle properties
    * output: new position, new direction of particle
@@ -93,6 +73,7 @@ public:
   }
 
 }; // end class declaration
+
 
 /**
  * this function propagates the track along the "helix-solution" by a step step
@@ -109,17 +90,16 @@ void ConstBzFieldStepper::DoStep(
     BaseDType & dx, BaseDType & dy, BaseDType & dz
     ) const
 {
-  constexpr BaseDType kB2C_local =  -0.299792458 * ( copcore::units::GeV / (copcore::units::tesla * copcore::units::meter) ) ; // -0.299792458e-3;
   const double kSmall     = 1.E-30;
   // could do a fast square root here
   BaseDType dt      = sqrt((dx0 * dx0) + (dy0 * dy0)) + kSmall;
   BaseDType invnorm = 1. / dt;
   // radius has sign and determines the sense of rotation
-  BaseDType R = momentum * dt / ((kB2C_local * BaseDType(charge)) * (fBz));
+  BaseDType R = momentum * dt / ((BaseDType(kB2C) * BaseDType(charge)) * (fBz));
 
   BaseDType cosa = dx0 * invnorm;
   BaseDType sina = dy0 * invnorm;
-  BaseDType phi  = step * BaseDType(charge) * fBz * kB2C_local / momentum;
+  BaseDType phi  = step * BaseDType(charge) * fBz * BaseDType(kB2C) / momentum;
 
   BaseDType cosphi;
   BaseDType sinphi;
@@ -134,66 +114,5 @@ void ConstBzFieldStepper::DoStep(
   dz = dz0;
 }
 
-/**
- * basket version of dostep
- */
-/*
- SW: commented out due to explicit Vc dependence and since it is not currently used
-     leaving the code here to show how one would dispatch to the kernel with Vc
-#define _R_ __restrict__
-void ConstBzFieldStepper::DoStep_v(
-                      double const * _R_ posx, double const * _R_ posy, double const * _R_ posz,
-                      double const * _R_ dirx, double const * _R_ diry, double const * _R_ dirz,
-                      int const * _R_ charge, double const * _R_ momentum, double const * _R_ step,
-                      double * _R_ newposx, double * _R_ newposy, double * _R_ newposz,
-                      double * _R_ newdirx, double * _R_ newdiry, double * _R_ newdirz,
-                      int np
-                   ) const
- {
-     // we have choice here: ( try autovectorization: )
-
-//#pragma ivdep
-//      for (int i=0;i<np;++i){
-//            DoStep( posx[i], posy[i], posz[i], dirx[i], diry[i], dirz[i],
-//                    charge[i], momentum[i], step[i],
-//                    newposx[i], newposy[i], newposz[i],
-//                    newdirx[i], newdiry[i], newdirz[i]
-//                  );
-//       }
-
-     // alternative loop with Vc:
-
-     for (int i=0;i<np;i+=Double_v::Size)
-     {
-          // results cannot not be temporaries
-          Double_v newposx_v, newposy_v, newposz_v,
-                     newdirx_v, newdiry_v,newdirz_v;
-          DoStep( Double_v(posx[i]), Double_v(posy[i]), Double_v(posz[i]),
-                  Double_v(dirx[i]), Double_v(diry[i]), Double_v(dirz[i]),
-                  geant::Int_v(charge[i]), Double_v(momentum[i]), Double_v(step[i]),
-                  newposx_v,
-                  newposy_v,
-                  newposz_v,
-                  newdirx_v,
-                  newdiry_v,
-                  newdirz_v
-                 );
-          // write results
-          newposx_v.store(&newposx[i]);
-          newposy_v.store(&newposy[i]);
-          newposz_v.store(&newposz[i]);
-          newdirx_v.store(&newdirx[i]);
-          newdiry_v.store(&newdiry[i]);
-          newdirz_v.store(&newdirz[i]);
-     }
-     // tail part: tobedone
- }
-*/
-
-// TODO: above stepper is tailored/specialized to B=(0,0,Bz) in the global frame of reference
-// might need to provide more general class in which the constant field has arbitrary direction
-
-// } // namespace GEANT_IMPL_NAMESPACE
-// } // namespace geant
 
 #endif /* CONSTBzFIELDSTEPPER_H_ */
