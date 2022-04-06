572ba7a9ea92a20e407e24c006102628a16080be
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2022-04-06 18:24:01 +0200

    update G4HepEM

--- examples/Example6/example6.cu
+++ examples/Example6/example6.cu
@@ -31,6 +31,7 @@
 // Pull in implementation.
 #include <G4HepEmRunUtils.icc>
 #include <G4HepEmInteractionUtils.icc>
+#include <G4HepEmElectronEnergyLossFluctuation.icc>
 #include <G4HepEmElectronManager.icc>
 #include <G4HepEmElectronInteractionBrem.icc>
 #include <G4HepEmElectronInteractionIoni.icc>
