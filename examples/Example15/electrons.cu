0c5408529eda17f0382423046951f84239c548fb
John Apostolakis <john.apostolakis@cern.ch>
2022-06-30 11:25:20 +0200

    15/electrons: Improved printing of differences RK vs helix

--- examples/Example15/electrons.cu
+++ examples/Example15/electrons.cu
@@ -8,6 +8,8 @@
 #define VERBOSE  1
 #define USE_RK 1
 
+
+
 #ifdef  USE_RK
 // Classes for Runge-Kutta integration 
 #include "MagneticFieldEquation.h"
@@ -58,25 +60,29 @@ bool  CompareResponseVector3D(
 {
    bool bad = false; // Good ..
    Precision magOrig= originalVec.Mag();
-   vecgeom::Vector3D<Precision> diffBase = baselineVec-originalVec;
-   vecgeom::Vector3D<Precision> diffRes  = resultVec-originalVec;
-   Precision magDiffBase = diffBase.Mag();
-   Precision magDiffRes  = diffRes.Mag();
+   vecgeom::Vector3D<Precision> moveBase = baselineVec-originalVec;
+   vecgeom::Vector3D<Precision> moveRes  = resultVec-originalVec;
+   Precision magMoveBase = moveBase.Mag();
+   Precision magDiffRes  = moveRes.Mag();
 
-   if ( std::fabs( magDiffRes / magDiffBase) - 1.0 > thresholdRel
+   if ( std::fabs( magDiffRes / magMoveBase) - 1.0 > thresholdRel
         || 
-        ( resultVec - baselineVec ).Mag() > thresholdRel * magDiffBase 
+        ( resultVec - baselineVec ).Mag() > thresholdRel * magMoveBase 
       ){
       // printf("Difference seen in vector %s : ", vecName );
-      printf(" id %3d - Diff in %s: df_Res/df_Base-1 = %7.3g | base-orig:  mag= %9.4g v3= %14.9f %14.9f %14.9f  || new-original: mag= %9.4g || new-base= %14.9g %14.9g %14.9g (mag= %14.9g) || origVec= %14.9f %14.9f %14.9f (mag=%14.9f) || base= %14.9f %14.9f %14.9f (mag=%9.4g) || d/newVec= %14.9f %14.9f %14.9f (mag = %14.9g)\n",
-             id, vecName,  (diffRes.Mag() / diffBase.Mag() - 1.0),
-             diffBase.Mag(), diffBase[0], diffBase[1], diffBase[2],
+      printf(" id %3d - Diff in %s: "
+             " new-base= %14.9g %14.9g %14.9g (mag= %14.9g) "
+             " mv_Res/mv_Base-1 = %7.3g | mv/base: 3v= %14.9f %14.9f %14.9f (mag= %9.4g)"
+             " | mv-new: 3v= %14.9f %14.9f %14.9f (mag = %14.9g)"
+             " || origVec= %14.9f %14.9f %14.9f (mag=%14.9f) | base= %14.9f %14.9f %14.9f (mag=%9.4g) \n",
+             id, vecName,
+             resultVec[0]-baselineVec[0], resultVec[1]-baselineVec[1], resultVec[2]-baselineVec[2], (resultVec-baselineVec).Mag(),             
+             (moveRes.Mag() / moveBase.Mag() - 1.0),
+             moveBase[0], moveBase[1], moveBase[2], moveBase.Mag(),
 //      printf("   new-original: mag= %20.16g ,  new_vec= %14.9f , %14.9f , %14.9f \n",
-             diffRes.Mag(), 
-             resultVec[0]-baselineVec[0], resultVec[1]-baselineVec[1], resultVec[2]-baselineVec[2], (resultVec-baselineVec).Mag(),
+             moveRes[0], moveRes[1], moveRes[2], moveRes.Mag(),
              originalVec[0], originalVec[1], originalVec[2], originalVec.Mag(),
-             baselineVec[0], baselineVec[1], baselineVec[2], baselineVec.Mag(),     
-             diffRes[0], diffRes[1], diffRes[2], diffRes.Mag() // );
+             baselineVec[0], baselineVec[1], baselineVec[2], baselineVec.Mag()     
          );      
       bad= true;
    }
@@ -238,7 +244,7 @@ static __device__ __forceinline__ void TransportElectrons(Track *electrons, cons
 #ifdef CHECK_RESULTS
       constexpr Precision thesholdDiff=3.0e-5;
       if( std::fabs( helixStepLength - geometryStepLength ) > 1.0e-4 * helixStepLength ) {
-        printf ("s: i= %3d phys-request= %8.4g  helix-did= %8.4g rk-did= %8.4g\n", i, geometricalStepLengthFromPhysics, helixStepLength, geometryStepLength);
+        printf ("s-len diff: i= %3d phys-request= %8.4g  helix-did= %8.4g rk-did= %8.4g\n", i, geometricalStepLengthFromPhysics, helixStepLength, geometryStepLength);
       }
       bool badPosition = 
         CompareResponseVector3D( i, startPosition, positionHx, currentTrack.pos, "Position", thesholdDiff );
