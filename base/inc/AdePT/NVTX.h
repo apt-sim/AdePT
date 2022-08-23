ebd96f0649dfc96b282513d55e623713db806091
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2022-08-23 17:03:09 +0200

    Add a SplitKernels example (#216)
    * Add automatic occupancy annotations to NVTX tracer.
    
    * Add example19 as a copy of example18.
    
    In addition: Remove old NVTX tracing code.
    
    * [ex19] Update README.
    
    * [ex19] Use cudaDeviceProp to allocate more memory.
    
    Co-authored by Guilherme Amadio <guilherme.amadio@cern.ch>
    
    * [ex19] Add InteractionKernel template.
    
    * [ex19] Move discrete interactions into dedicated kernels.
    
    * [ex19] Increase number of blocks.
    
    * [ex19] Don't advance the RNG before discrete interactions.
    
    Jonas pointed out the advance happens anyway when RNG states are
    branched, so no need to do it here.
    
    * [ex19] Fix default batch size to 52.
    
    Jonas asked for a batch size that's comparable with other examples.
    Using the default capacity of the containers in example18, one arrives
    at 52 primaries per batch.
    
    * [ex19] Launch all interaction kernels with transportBlocks blocks.
    
    On Jonas' request, use transportBlocks blocks as a starting point for all
    interaction kernels.

--- base/inc/AdePT/NVTX.h
+++ base/inc/AdePT/NVTX.h
@@ -9,10 +9,11 @@
 #ifndef NVTX_H
 #define NVTX_H
 
-#if defined USE_NVTX && !defined __CUDACC__
+#if defined USE_NVTX
 
 #include "nvToolsExt.h"
 
+#include <numeric>
 #include <array>
 #include <cstdint>
 #include <string>
@@ -22,9 +23,11 @@ class NVTXTracer {
                                                        0xff00ffff, 0xffff0000, 0xffffffff};
   std::string _name;
   nvtxRangeId_t _id;
+  std::array<unsigned long, 10> _lastOccups;
+  decltype(_lastOccups)::iterator _occupIt = _lastOccups.begin();
 
 public:
-  NVTXTracer(const char *name) : _name(name)
+  NVTXTracer(const char *name) : _name{name}
   {
     nvtxEventAttributes_t eventAttrib = {0};
     eventAttrib.version               = NVTX_VERSION;
@@ -34,10 +37,11 @@ public:
     eventAttrib.messageType           = NVTX_MESSAGE_TYPE_ASCII;
     eventAttrib.message.ascii         = name;
     _id                               = nvtxRangeStartEx(&eventAttrib);
+    _lastOccups.fill(0);
   }
   ~NVTXTracer() { nvtxRangeEnd(_id); }
 
-  __host__ void setTag(const char *name)
+  void setTag(const char *name)
   {
     if (_name == name) return;
 
@@ -54,6 +58,25 @@ public:
     _id                               = nvtxRangeStartEx(&eventAttrib);
   }
 
+  void setOccupancy(unsigned long occupancy)
+  {
+    // Require the occupancy to be larger than the majority of previous iterations to call it rising
+    const bool rising =
+        2 * std::count_if(_lastOccups.begin(), _lastOccups.end(), [occupancy](auto elm) { return occupancy > elm + 1; }) >
+        _lastOccups.size();
+
+    if (rising) {
+      setTag("occupancy rising");
+    } else if (_name == "occupancy rising") {
+      setTag("peak occupancy");
+    } else {
+      setTag("occupancy falling");
+    }
+
+    *_occupIt = occupancy;
+    if (++_occupIt == _lastOccups.end()) _occupIt = _lastOccups.begin();
+  }
+
   static uint32_t nextColour()
   {
     static int colour = 0;
@@ -68,6 +91,7 @@ class NVTXTracer {
 public:
   NVTXTracer(const char *) {}
   void setTag(const char *) {}
+  void setOccupancy(unsigned long) {}
 };
 
 #endif
