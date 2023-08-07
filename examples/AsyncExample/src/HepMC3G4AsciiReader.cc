1c361d406a6bc0091f8d2fd268fbc5be24601668
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2023-08-07 17:53:30 +0200

    [async] Remove static variables from HepMC3G4Interface
    - Make HepMC3 event inputs thread safe by removing all static data.
    - Read HepMC3 files locally in each thread.

--- examples/AsyncExample/src/HepMC3G4AsciiReader.cc
+++ examples/AsyncExample/src/HepMC3G4AsciiReader.cc
@@ -1,72 +1,41 @@
-// SPDX-FileCopyrightText: 2022 CERN
+// SPDX-FileCopyrightText: 2023 CERN
 // SPDX-License-Identifier: Apache-2.0
 
 //
-/// \file eventgenerator/HepMC3/HepMCEx01/src/HepMC3G4AsciiReader.cc
 /// \brief Implementation of the HepMC3G4AsciiReader class
 //
 //
 
 #include "HepMC3G4AsciiReader.hh"
 #include "HepMC3G4AsciiReaderMessenger.hh"
-#include "G4EventManager.hh"
-#include "G4Event.hh"
 
 #include <iostream>
-#include <fstream>
-
-std::vector<HepMC3::GenEvent*>* HepMC3G4AsciiReader::fEvents = nullptr;
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 HepMC3G4AsciiReader::HepMC3G4AsciiReader()
-  :  filename("xxx.dat"), verbose(0)
+    : event{new HepMC3::GenEvent()}, messenger{new HepMC3G4AsciiReaderMessenger(this)}
 {
-  messenger = new HepMC3G4AsciiReaderMessenger(this);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-HepMC3G4AsciiReader::~HepMC3G4AsciiReader()
-{
-  for (auto evt:*fEvents) delete evt;
-
-  delete fEvents;
-  delete asciiInput;
-  delete messenger;
-}
+HepMC3G4AsciiReader::~HepMC3G4AsciiReader() {}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-void HepMC3G4AsciiReader::Initialize()
+HepMC3::GenEvent *HepMC3G4AsciiReader::GenerateHepMCEvent(int eventId)
 {
-  asciiInput = new HepMC3::ReaderAscii(filename.c_str());
+  if (eventId >= maxNumberOfEvents) return nullptr;
+  if (!asciiInput) asciiInput.reset(new HepMC3::ReaderAscii(filename.c_str()));
 
-  fEvents = new std::vector<HepMC3::GenEvent*>();
-  HepMC3::GenEvent* evt = nullptr;
-  
-  int counter = 0;
+  const auto advance = eventId - previousEventNumber;
+  if (advance > 1) asciiInput->skip(advance - 1);
 
-  if (firstEventNumber > 0) asciiInput->skip(firstEventNumber);
-  
-  // read file 
-  while(!asciiInput->failed() && counter < maxNumberOfEvents)
-  {
-    evt = new HepMC3::GenEvent();
-    asciiInput->read_event(*evt);
+  event->clear();
+  asciiInput->read_event(*event);
 
-    if (asciiInput->failed()) break;
+  previousEventNumber = eventId;
+  if (asciiInput->failed()) return nullptr;
 
-    fEvents->push_back(evt);
-    counter++;
-  }
-  G4cout << "Read " << filename << " file with " << fEvents->size() 
-  << " events." << G4endl;
-}
+  if (verbose > 0) HepMC3::Print::content(*event);
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-HepMC3::GenEvent* HepMC3G4AsciiReader::GenerateHepMCEvent(int eventId)
-{
-  HepMC3::GenEvent* evt = (*fEvents)[(eventId % fEvents->size())];
-
-  if(verbose>0) HepMC3::Print::content(*evt);
-
-  return evt;
+  return event.get();
 }
