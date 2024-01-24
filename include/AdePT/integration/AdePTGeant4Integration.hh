bfe9ff209dc79cb7b844bd1c95f02a7350eb3591
JuanGonzalezCaminero <j.gonzalez@cern.ch>
2024-01-24 14:03:15 +0100

    tmp

---
+++ include/AdePT/integration/AdePTGeant4Integration.hh
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2022 CERN
+// SPDX-License-Identifier: Apache-2.0
+
+///   The AdePT-Geant4 integration layer
+///   - Initialization and checking of VecGeom geometry
+///   - G4Hit reconstruction from GPU hits
+///   - Processing of reconstructed hits using the G4 Sensitive Detector
+
+
+class AdePTGeant4Integration {
+    public: 
+    
+    private:
+        
+}
+
+
+
