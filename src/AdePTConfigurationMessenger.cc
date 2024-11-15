e7b0aa9e0d84b975290bbd0d5843fa69eaf3317f
SeverinDiederichs <severin.diederichs@cern.ch>
2024-11-15 16:34:45 +0100

    add covfie accessor

--- src/AdePTConfigurationMessenger.cc
+++ src/AdePTConfigurationMessenger.cc
@@ -68,6 +68,9 @@ AdePTConfigurationMessenger::AdePTConfigurationMessenger(AdePTConfiguration *ade
   fSetGDMLCmd = new G4UIcmdWithAString("/adept/setVecGeomGDML", this);
   fSetGDMLCmd->SetGuidance("Temporary method for setting the geometry to use with VecGeom");
 
+  fSetCovfieFileCmd = new G4UIcmdWithAString("/adept/setCovfieBfieldFile", this);
+  fSetCovfieFileCmd->SetGuidance("Set the path the the covfie file for reading in an external magnetic field");
+
   fSetCUDAStackLimitCmd = new G4UIcmdWithAnInteger("/adept/setCUDAStackLimit", this);
   fSetCUDAStackLimitCmd->SetGuidance("Set the CUDA device stack limit");
   fSetCUDAHeapLimitCmd = new G4UIcmdWithAnInteger("/adept/setCUDAHeapLimit", this);
@@ -91,6 +94,9 @@ AdePTConfigurationMessenger::~AdePTConfigurationMessenger()
   delete fSetMillionsOfTrackSlotsCmd;
   delete fSetMillionsOfHitSlotsCmd;
   delete fSetHitBufferFlushThresholdCmd;
+  delete fSetGDMLCmd;
+  delete fSetCovfieFileCmd;
+  delete fSetCUDAStackLimitCmd;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -120,6 +126,8 @@ void AdePTConfigurationMessenger::SetNewValue(G4UIcommand *command, G4String new
     fAdePTConfiguration->SetHitBufferFlushThreshold(fSetHitBufferFlushThresholdCmd->GetNewDoubleValue(newValue));
   } else if (command == fSetGDMLCmd) {
     fAdePTConfiguration->SetVecGeomGDML(newValue);
+  } else if (command == fSetCovfieFileCmd) {
+    fAdePTConfiguration->SetCovfieBfieldFile(newValue);
   } else if (command == fSetCUDAStackLimitCmd) {
     fAdePTConfiguration->SetCUDAStackLimit(fSetCUDAStackLimitCmd->GetNewIntValue(newValue));
   } else if (command == fSetCUDAHeapLimitCmd) {
