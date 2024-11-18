8bdec5e21049dbe7a51e3958098611b0a1cc809a
SeverinDiederichs <severin.diederichs@cern.ch>
2024-11-18 12:44:05 +0100

    more protection

--- include/AdePT/core/AdePTTransportStruct.cuh
+++ include/AdePT/core/AdePTTransportStruct.cuh
@@ -93,6 +93,8 @@ extern __constant__ struct G4HepEmData g4HepEmData;
 extern __constant__ __device__ adeptint::VolAuxData *gVolAuxData;
 extern __constant__ __device__ double BzFieldValue;
 extern __constant__ __device__ bool ApplyCuts;
+#ifdef ADEPT_USE_EXT_BFIELD
 __constant__ __device__ typename cuda_field_t::view_t *MagneticFieldView = nullptr;
+#endif 
 } // namespace adept_impl
 #endif
