f046f305be6307afc8b1f0d4131365de977263ab
SeverinDiederichs <severin.diederichs@cern.ch>
2024-11-21 16:27:07 +0100

    fix RunkeKutta integration

--- src/AdePTConfigurationMessenger.cc
+++ src/AdePTConfigurationMessenger.cc
@@ -96,7 +96,7 @@ AdePTConfigurationMessenger::~AdePTConfigurationMessenger()
   delete fSetHitBufferFlushThresholdCmd;
   delete fSetGDMLCmd;
   delete fSetCovfieFileCmd;
-  delete fSetCUDAStackLimitCmd;
+  // delete fSetCUDAStackLimitCmd;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
