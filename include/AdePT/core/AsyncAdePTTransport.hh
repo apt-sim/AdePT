efddfc1b39aec6c7344dc656275adf9a35f39e84
SeverinDiederichs <severin.diederichs@cern.ch>
2024-12-02 18:43:33 +0100

    add things to async example to make it compile (fails the linking due to other reasons)

--- include/AdePT/core/AsyncAdePTTransport.hh
+++ include/AdePT/core/AsyncAdePTTransport.hh
@@ -62,7 +62,9 @@ private:
   // Flags for the kernels to return the last or all steps, needed for PostUserTrackingAction or UserSteppingAction
   bool fReturnAllSteps = false;
   bool fReturnLastStep = false;
-
+  std::string fBfieldFile{""};         ///< Path to magnetic field file (in the covfie format)
+  //GeneralMagneticField fMagneticField; ///< arbitrary magnetic field
+  
   void Initialize();
   void InitBVH();
   bool InitializeField(double bz);
@@ -93,6 +95,8 @@ public:
   void SetTrackInAllRegions(bool trackInAllRegions) override { fTrackInAllRegions = trackInAllRegions; }
   bool GetTrackInAllRegions() const override { return fTrackInAllRegions; }
   void SetGPURegionNames(std::vector<std::string> const *regionNames) override { fGPURegionNames = regionNames; }
+  /// @brief Set path to covfie Bfield file
+  void SetBfieldFileName(const std::string &fileName) override { fBfieldFile = fileName; }
   void SetCUDAStackLimit(int limit) override {};
   void SetCUDAHeapLimit(int limit) override {};
   std::vector<std::string> const *GetGPURegionNames() override { return fGPURegionNames; }
