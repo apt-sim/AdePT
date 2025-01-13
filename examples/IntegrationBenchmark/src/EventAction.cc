526a1c799eaa7bc33f11713916ae72634a76090d
SeverinDiederichs <severin.diederichs@cern.ch>
2025-01-13 14:36:59 +0100

    fix accumulated events output

--- examples/IntegrationBenchmark/src/EventAction.cc
+++ examples/IntegrationBenchmark/src/EventAction.cc
@@ -155,10 +155,10 @@ void EventAction::EndOfEventAction(const G4Event *aEvent)
       // Write data to output file. Validation data can take a lot of memory, and we don't need to aggregate
       // the results of multiple events at runtime, so for better performance it's easier to write the output here
       aTestManager->exportCSV(false);
-    } else {
-      // overwrite to have all validation data written into a single line for all events
-      aTestManager->exportCSV(true);
-    }
+    } // else {
+      //  overwrite to have all validation data written into a single line for all events
+    //   aTestManager->exportCSV(false);
+    // }
 
     // Store test manager
     // TestManagerStore<int>::GetInstance()->RecordState(aTestManager);
--- examples/IntegrationBenchmark/src/RunAction.cc
+++ examples/IntegrationBenchmark/src/RunAction.cc
@@ -68,6 +68,12 @@ void RunAction::EndOfRunAction(const G4Run *)
   auto tid  = G4Threading::G4GetThreadId();
   // Just protect the printout to avoid interlacing text
   const std::lock_guard<std::mutex> lock(print_mutex);
+
+  if (GetDoValidation() && GetDoAccumulatedEvents()) {
+    // overwrite to have all validation data written into a single line for all events
+    fRun->GetTestManager()->exportCSV(false);
+  }
+
   // Print timer just for the master thread since this is called when all workers are done
   if (tid < 0) {
     G4cout << "Run time: " << time << "\n";
