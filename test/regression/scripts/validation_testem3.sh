f1b0dd5b20f718302c8bf500d1ea118a391c354a
Juan González <j.gonzalez@cern.ch>
2025-04-02 09:01:18 +0200

    Cleanup of tests (#373)
    Moving `IntegrationBenchmark` into the `test` directory as it is not
    meant to be an example. Moving old tests into `test/unit` and the
    current validation and reproducibility into `test/regression` so that
    everything is registered in the same place.
    
    Changed the test scripts to output into a temporary directory to make
    cleanup less error-prone

--- test/regression/scripts/validation_testem3.sh
+++ test/regression/scripts/validation_testem3.sh
@@ -14,14 +14,13 @@ set -eu -o pipefail
 ADEPT_EXECUTABLE=$1
 PROJECT_BINARY_DIR=$2
 PROJECT_SOURCE_DIR=$3
-
-CI_TEST_DIR=${PROJECT_SOURCE_DIR}/examples/IntegrationBenchmark/ci_tests
+CI_TEST_DIR=$4
+CI_TMP_DIR=$5
 
 # Define cleanup function of temporary files
 cleanup() {
   echo "Cleaning up temporary files..."
-  rm -f ${CI_TEST_DIR}/adept_em3_1e5_e-.csv ${CI_TEST_DIR}/adept_em3_1e5_e-_global.csv \
-        ${CI_TEST_DIR}/validation_testem3.mac
+  rm -rf ${CI_TMP_DIR}
 }
 
 # register cleanup to be called on exit
@@ -29,10 +28,13 @@ trap cleanup EXIT
 # called it directly ensure clean environment
 cleanup
 
+# Create temporary directory
+mkdir -p ${CI_TMP_DIR}
+
 # use gun_type hepmc or setDefault
 $CI_TEST_DIR/python_scripts/macro_generator.py \
     --template ${CI_TEST_DIR}/example_template.mac \
-    --output ${CI_TEST_DIR}/validation_testem3.mac \
+    --output ${CI_TMP_DIR}/validation_testem3.mac \
     --gdml_name ${PROJECT_SOURCE_DIR}/examples/data/testEm3.gdml \
     --num_threads 4 \
     --num_events 1000 \
@@ -43,15 +45,15 @@ $CI_TEST_DIR/python_scripts/macro_generator.py \
 
 # run test
 $ADEPT_EXECUTABLE --do_validation --allsensitive --accumulated_events \
-                  -m "${CI_TEST_DIR}/validation_testem3.mac" \
-                  --output_dir "${CI_TEST_DIR}" \
+                  -m "${CI_TMP_DIR}/validation_testem3.mac" \
+                  --output_dir "${CI_TMP_DIR}" \
                   --output_file "adept_em3_1e5_e-"
 
 # Validating the relative error per layer
 # The saved G4 benchmark file was run with --noadept and 1e7 primary 10 GeV electrons.
 # Comparing against 1e5 primary electrons (500 per event, 200 events) with AdePT should give errors below 1%.
 # This is a compromise between run time and accuracy of the test 
-$CI_TEST_DIR/python_scripts/check_validation.py --file1 ${CI_TEST_DIR}/adept_em3_1e5_e-.csv \
+$CI_TEST_DIR/python_scripts/check_validation.py --file1 ${CI_TMP_DIR}/adept_em3_1e5_e-.csv \
                                                 --file2 ${CI_TEST_DIR}/benchmark_files/g4_em3_10e7_e-.csv \
                                                 --n1 1e5 --n2 1e7 --tol 0.01 \
                                                 # --plot_file plot.png # uncomment to plot the validation plot
