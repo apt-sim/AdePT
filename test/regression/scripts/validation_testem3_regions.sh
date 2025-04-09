78836ea2769f3cd12c13751dfb251f6849791225
JuanGonzalezCaminero <j.gonzalez@cern.ch>
2025-04-09 15:35:45 +0200

    Adjust test parameters, increase leak buffer size

--- test/regression/scripts/validation_testem3_regions.sh
+++ test/regression/scripts/validation_testem3_regions.sh
@@ -38,14 +38,12 @@ $CI_TEST_DIR/python_scripts/macro_generator.py \
     --gdml_name ${PROJECT_SOURCE_DIR}/examples/data/testEm3_regions.gdml \
     --num_threads 4 \
     --num_events 1000 \
-    --num_trackslots 3 \
-    --num_hitslots 22 \
+    --num_trackslots 2 \
+    --num_hitslots 18 \
     --track_in_all_regions False\
     --gun_type setDefault\
     --regions "Layer1, Layer2, Layer3, Layer4, Layer5, Layer6, Layer7, Layer8, Layer9, Layer10,\
-            Layer11, Layer12, Layer13, Layer14, Layer15, Layer16, Layer17, Layer18, Layer19, Layer20,\
-            Layer31, Layer32, Layer33, Layer34, Layer35, Layer36, Layer37, Layer38, Layer39, Layer40,\
-            Layer41, Layer42, Layer43, Layer44, Layer45, Layer46, Layer47, Layer48, Layer49, Layer50"
+            Layer11, Layer12, Layer13, Layer14, Layer15, Layer16, Layer17, Layer18, Layer19, Layer20"
 
 # run test
 $ADEPT_EXECUTABLE --do_validation --allsensitive --accumulated_events \
