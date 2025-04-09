9e227f1a0c9357c4c0363c59bf43b705df6df9a2
JuanGonzalezCaminero <j.gonzalez@cern.ch>
2025-04-09 15:52:12 +0200

    Adjust test parameters, increase leak buffer size

--- test/regression/scripts/validation_testem3_regions.sh
+++ test/regression/scripts/validation_testem3_regions.sh
@@ -43,7 +43,7 @@ $CI_TEST_DIR/python_scripts/macro_generator.py \
     --track_in_all_regions False\
     --gun_type setDefault\
     --regions "Layer1, Layer2, Layer3, Layer4, Layer5, Layer6, Layer7, Layer8, Layer9, Layer10,\
-            Layer11, Layer12, Layer13, Layer14, Layer15, Layer16, Layer17, Layer18, Layer19, Layer20\
+            Layer11, Layer12, Layer13, Layer14, Layer15, Layer16, Layer17, Layer18, Layer19, Layer20,\
             Layer21, Layer22, Layer23, Layer24, Layer25, Layer26, Layer27, Layer28, Layer29, Layer30"
 
 # run test
