1ae5c08642750dd8133e8a7dc7b5c4c6562a8d79
SeverinDiederichs <severin.diederichs@cern.ch>
2025-06-26 17:29:55 +0200

    use 1. branched state when 3 secondaries can be generated

--- include/AdePT/kernels/electrons_split.cuh
+++ include/AdePT/kernels/electrons_split.cuh
@@ -589,10 +589,9 @@ __device__ __forceinline__ void PerformStoppedAnnihilation(const int slot, Track
       double sinPhi, cosPhi;
       sincos(phi, &sinPhi, &cosPhi);
 
-      // Perform the discrete interaction, branch a new RNG state with advance so it is
-      // ready to be used. As another state may have already branched from the current rngState, advance it first
+      // as the other branched newRNG may have already been used by interactions before, we need to advance and create a new one
       currentTrack.rngState.Advance();
-      auto newRNG = RanluxppDouble(currentTrack.rngState.Branch());
+      RanluxppDouble newRNG(currentTrack.rngState.Branch());
 
       Track &gamma1 = secondaries.gammas.NextTrack(newRNG, double{copcore::units::kElectronMassC2}, currentTrack.pos,
                                                    vecgeom::Vector3D<Precision>{sint * cosPhi, sint * sinPhi, cost},
