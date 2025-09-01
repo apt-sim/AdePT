898d5d91dbafbe6b9699ec29c662a18ee2c8b3a3
SeverinDiederichs <severin.diederichs@cern.ch>
2025-09-01 19:08:30 +0200

    Revert "add compilation guard"
    This reverts commit 6f279e5473e3615e81c285bfee0004929a09ea6b.

--- include/AdePT/integration/AdePTTrackingManager.hh
+++ include/AdePT/integration/AdePTTrackingManager.hh
@@ -22,10 +22,6 @@
 #include <memory>
 #include <vector>
 
-#ifndef ASYNC_MODE
-#  error "ASYNC_MODE is not defined."
-#endif
-
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 class AdePTTrackingManager : public G4VTrackingManager {
